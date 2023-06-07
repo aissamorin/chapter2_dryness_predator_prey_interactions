c7a16601e301be33a6df9f06bc9d9d11fc3c894f
Aissa Morin <morinaissa@gmail.com>
2023-06-07 18:31:53 +0200

    Added larger sample dataset exploration (using existing functions)

--- make.R
+++ make.R
@@ -183,7 +183,7 @@ gs_data %>%
   dplyr::filter(carcass_species %in% c('buffalo', 'nyala', 'warthog')) %>%
 
   boxplot_main_sp( tab = .,
-                   varx= 'cluster_month',
+                   varx= 'season1',
                    vary= 'mean_fat_rate',
                    species_vec = c('buffalo', 'nyala', 'warthog'))
 
@@ -195,6 +195,124 @@ gs_data %>%
 
 ls_data <- get_ls_data(tab= clean_fat_data,
                        save = F)
+
+#> carcass number summaries ####
+
+
+# per species
+
+get_nb_carcass_table(ls_data,
+                     carcass_species) #... = carcass_species
+
+#per cluster month
+get_nb_carcass_table(ls_data,
+                     cluster_month) #... = cluster_month
+
+#per specie & per month
+get_nb_carcass_table(ls_data,
+                     carcass_species,cluster_month) #... = cluster_month
+
+#per season 1
+get_nb_carcass_table(ls_data,
+                     season1) #... = season1
+
+
+
+#per species & season 1
+get_nb_carcass_table(ls_data,
+                     carcass_species,season1) #... = carcass_species, season1
+
+
+
+#> Boxplots ####
+
+#>> Per month ####
+
+#fat rate value ~cluster month all sp
+
+boxplot_all_sp (tab = ls_data,
+                varx = 'cluster_month' ,
+                vary = 'sample_replicate_fat_rate' )
+
+
+#fat rate value ~cluster month main sp
+boxplot_main_sp(tab = ls_data,
+                varx = 'cluster_month' ,
+                vary ='sample_replicate_fat_rate',
+                species_vec = c('buffalo', 'nyala', 'warthog'))
+
+
+# mean fat rate ~ cluster_month all sp
+
+# data preparation, compute the mean fat rate per sample_ID
+ls_data %>%
+  dplyr::group_by(samples_ID, cluster_start_date, cluster_month) %>%
+  dplyr::summarise(mean_fat_rate = mean(sample_replicate_fat_rate)) %>%
+
+  boxplot_all_sp (tab = .,
+                  varx = 'cluster_month' ,
+                  vary = 'mean_fat_rate' )
+
+# mean fat rate ~ cluster_month main sp
+
+# data preparation, compute the mean fat rate per sample_ID & per species
+
+ls_data %>%
+  dplyr::group_by(samples_ID, cluster_start_date, cluster_month, carcass_species ) %>%
+  dplyr::summarise(mean_fat_rate = mean(sample_replicate_fat_rate)) %>%
+  #dplyr::filter(carcass_species %in% c('buffalo', 'nyala', 'warthog')) %>%
+
+  boxplot_main_sp( tab = .,
+                   varx= 'cluster_month',
+                   vary= 'mean_fat_rate',
+                   species_vec = c('buffalo', 'nyala', 'warthog'))
+
+###>> Per Season ####
+
+# since season 1 is more balanced than season 2 --> use season 1
+
+#fat rate value ~ season1 (early dry = 2-6 & dry = 7-11), all sp
+
+boxplot_all_sp (tab = ls_data,
+                varx = 'season1' ,
+                vary = 'sample_replicate_fat_rate' )
+
+# fat rate value ~ season1 (early dry = 2-6 & dry = 7-11), main sp
+
+boxplot_main_sp(tab = ls_data,
+                varx = 'season1' ,
+                vary ='sample_replicate_fat_rate',
+                species_vec = c('buffalo', 'nyala', 'warthog'))
+
+
+# mean fat rate ~ season 1 all sp
+
+# data preparation, compute the mean fat rate per sample_ID & per season1
+ls_data %>%
+  dplyr::group_by(samples_ID, cluster_start_date, cluster_month, season1) %>%
+  dplyr::summarise(mean_fat_rate = mean(sample_replicate_fat_rate)) %>%
+
+  boxplot_all_sp (tab = .,
+                  varx = 'season1' ,
+                  vary = 'mean_fat_rate' )
+
+
+
+# mean fat rate ~ season 1 main sp
+
+# data preparation, compute the mean fat rate per sample_ID, per season1 & per species
+
+ls_data %>%
+  dplyr::group_by(samples_ID, cluster_start_date, cluster_month, carcass_species, season1 ) %>%
+  dplyr::summarise(mean_fat_rate = mean(sample_replicate_fat_rate)) %>%
+  dplyr::filter(carcass_species %in% c('buffalo', 'nyala', 'warthog')) %>%
+
+  boxplot_main_sp( tab = .,
+                   varx= 'season1',
+                   vary= 'mean_fat_rate',
+                   species_vec = c('buffalo', 'nyala', 'warthog'))
+
+
 
 # II Body condition analyses ####
 
