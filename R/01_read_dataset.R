ccd7d5cee0d2b3cc0826d362a8cb0f93ab64686f
Aissa Morin <morinaissa@gmail.com>
2023-06-14 17:21:23 +0200

    Creating function to read prey available body condition data

--- R/01_read_dataset.R
+++ R/01_read_dataset.R
@@ -30,3 +30,17 @@ read_fat_data_raw_2 <- function() {
   readr::read_csv2(here::here('data','data_fat_analyses_08_06_23_raw.csv'))
 
 }
+
+
+
+
+#' Function aims at reading available prey body condition data for comparison with body condition used (i.e. preyed) by lions
+#'
+#' @return a table with the number of individual in each BC catagories (column) per seasons (raws)
+#' @export
+
+read_data_land_BC <- function() {
+
+  readr::read_csv2(here::here('data','data_landscape_BC.csv'))
+
+}
